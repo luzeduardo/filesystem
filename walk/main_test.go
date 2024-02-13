b75c8a0710d8d2446c240ade16a8d655e7d9c592
Eduardo Luz <luz.eduardo@gmail.com>
2024-02-13 15:46:03 +0000

    test archiving files flag

--- walk/main_test.go
+++ walk/main_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -25,6 +26,64 @@ func createTempDir(t *testing.T, files map[string]int) (dirname string, cleanup 
 		}
 	}
 	return tempDir, func() { os.RemoveAll(tempDir) }
+}
+
+func TestRunArchive(t *testing.T) {
+	testCases := []struct {
+		name         string
+		cfg          config
+		extNoArchive string
+		nArchive     int
+		nNoArchive   int
+	}{
+		{name: "ArchiveExtensionNoMatch", cfg: config{ext: ".log"}, extNoArchive: ".gz", nArchive: 10, nNoArchive: 0},
+		{name: "ArchiveExtensionMatch", cfg: config{ext: ".log"}, extNoArchive: ".gz", nArchive: 10, nNoArchive: 0},
+		{name: "ArchiveExtensionMixed", cfg: config{ext: ".log"}, extNoArchive: ".gz", nArchive: 5, nNoArchive: 5},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var buffer bytes.Buffer
+
+			tempDir, cleanup := createTempDir(t, map[string]int{
+				tc.cfg.ext:      tc.nArchive,
+				tc.extNoArchive: tc.nNoArchive,
+			})
+			defer cleanup()
+
+			archiveDir, cleanupArchive := createTempDir(t, nil)
+			defer cleanupArchive()
+
+			tc.cfg.archive = archiveDir
+
+			if err := run(tempDir, &buffer, tc.cfg); err != nil {
+				t.Fatal(err)
+			}
+
+			pattern := filepath.Join(tempDir, fmt.Sprintf("*%s", tc.cfg.ext))
+			expFiles, err := filepath.Glob(pattern)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			expOut := strings.Join(expFiles, "\n")
+
+			res := strings.TrimSpace(buffer.String())
+			if expOut != res {
+				t.Errorf("Expected %q, got %q instead \n", expOut, res)
+			}
+
+			filesArchived, err := os.ReadDir(archiveDir)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if len(filesArchived) != tc.nArchive {
+				t.Errorf("Expected %d files archived, got %d instead\n",
+					tc.nArchive, len(filesArchived))
+			}
+		})
+	}
 }
 
 func TestRunDelExtension(t *testing.T) {
